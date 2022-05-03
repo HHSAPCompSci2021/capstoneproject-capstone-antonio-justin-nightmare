8f90a2ff2166e8d4c0915f1f786027222475b621
Justin Yen <jyen454@student.fuhsd.org>
2022-05-03 10:47:05 -0700

    enemy methods

--- CapstoneProject/src/Enemy.java
+++ CapstoneProject/src/Enemy.java
@@ -1,4 +1,25 @@
+import java.awt.Point;
 
 public class Enemy {
-
+	private int health;
+	
+	public Enemy() {
+		health = 10;
+	} 
+	
+	public void act(){
+		
+	}
+	
+	private Point findPath() {
+		return null;
+	} 
+	
+	private void takeDamage() {
+		
+	}
+	
+	private void move() {
+		
+	}
 }
