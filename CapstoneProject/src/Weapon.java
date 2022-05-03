dfef7303fe18347cfc01f6858cf70954a283e3ac
Justin Yen <jyen454@student.fuhsd.org>
2022-05-03 10:50:07 -0700

    Update Weapon.java

--- CapstoneProject/src/Weapon.java
+++ CapstoneProject/src/Weapon.java
@@ -1,4 +1,18 @@
 
 public class Weapon {
-
+	int attackDamage;
+	double attackSpeed;
+	
+	public Weapon() {
+		attackDamage = 3;
+		attackSpeed = 1;
+	}
+	
+	public int getAttackDamage() {
+		return attackDamage;
+	}
+	
+	public double getAttackSpeed() {
+		return attackSpeed;
+	}
 }
