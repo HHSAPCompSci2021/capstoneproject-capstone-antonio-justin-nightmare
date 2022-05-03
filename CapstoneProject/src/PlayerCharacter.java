75b9105c2ab982ba11cd57fd551e812265cee48a
Justin Yen <jyen454@student.fuhsd.org>
2022-05-03 10:48:31 -0700

    Update PlayerCharacter.java

--- CapstoneProject/src/PlayerCharacter.java
+++ CapstoneProject/src/PlayerCharacter.java
@@ -1,4 +1,16 @@
 
 public class PlayerCharacter {
-
+	private Weapon weapon;
+	
+	public PlayerCharacter() {
+		weapon = new Weapon();
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void attack() {
+		
+	}
 }
