3a78a5f0459b465007c64d1f32593e9347343327
Justin Yen <jyen454@student.fuhsd.org>
2022-05-03 10:52:19 -0700

    Update Tower.java

--- CapstoneProject/src/Tower.java
+++ CapstoneProject/src/Tower.java
@@ -1,4 +1,19 @@
 
 public class Tower {
-
+	int price,attackDamage;
+	double attackSpeed;
+	
+	public Tower() {
+		price = 100;
+		attackDamage = 2;
+		attackSpeed = 1;
+	}
+	
+	public void act() {
+		attack();
+	}
+	
+	private void attack() {
+		
+	}
 }
