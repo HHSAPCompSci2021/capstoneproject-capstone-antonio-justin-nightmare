0ff7cd6e5509f1d0a9a2b3e3118c44c2822fda7a
Antonio Cuan <antonio.j.cuan@gmail.com>
2022-05-03 10:41:57 -0700

    added Main and DrawingSurface classes and processing library

--- CapstoneProject/src/Main.java
+++ CapstoneProject/src/Main.java
@@ -1,9 +1,29 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String args[]) {
 
+		DrawingSurface drawing = new DrawingSurface();
+		PApplet.runSketch(new String[]{""}, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+
+		window.setSize(1280, 720);
+		window.setMinimumSize(new Dimension(1280,720));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+
+		window.setVisible(true);
+		
+		
+		canvas.requestFocus();
 	}
 
 }
