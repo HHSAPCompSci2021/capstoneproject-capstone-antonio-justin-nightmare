12a2a73e0b5fe8b7f9181931c405a9d93350e481
Antonio Cuan <antonio.j.cuan@gmail.com>
2022-05-06 11:08:29 -0700

    alpha release
     created runnable jar file, added README and UML diagram, coded mouse over grid feature

--- CapstoneProject/src/screenElements/Grid.java
+++ CapstoneProject/src/screenElements/Grid.java
@@ -6,16 +6,21 @@ import core.DrawingSurface;
 import gameElements.*;
 
 public class Grid extends ScreenElement{
+	private int cols;
+	private int rows;
+	private static final int CELL_WIDTH = 10;
 	private ArrayList<Enemy> enemies;
 	public Grid(int x, int y, int width, int height) {
 		super(x,y,width,height);
+		cols = width/CELL_WIDTH;
+		rows = height/CELL_WIDTH;
 		enemies = new ArrayList<Enemy>();
 	}
 	
 	public void draw(DrawingSurface surface) {
 		surface.push();
 		// Sets the background
-		surface.fill(255,0,0);
+		surface.noFill();
 		surface.rect(x, y, width, height);
 		
 		// Draws all the enemies
@@ -44,5 +49,17 @@ public class Grid extends ScreenElement{
 	
 	public int getY() {
 		return y;
+	}
+	
+	public int getRows() {
+		return rows;
+	}
+	
+	public int getCols() {
+		return cols;
+	}
+	
+	public int getCellWidth() {
+		return CELL_WIDTH;
 	}
 }
--- CapstoneProject/src/screens/GameScreen.java
+++ CapstoneProject/src/screens/GameScreen.java
@@ -23,6 +23,7 @@ public class GameScreen extends Screen{
 
 	public void draw() {
 		surface.background(150,150,200);
+		fillGrid();
 		grid.draw(surface);
 		grid.next();
 		store.draw(surface);
@@ -33,6 +34,32 @@ public class GameScreen extends Screen{
 	private void processKeyPresses() {
 		if (surface.isPressed(KeyEvent.VK_W)) {
 			System.out.println("W pressed");
+		}
+	}
+	
+	private void fillGrid() {
+		float cellWidth = grid.getCellWidth();
+		
+		surface.fill(255);
+		surface.stroke(255);
+		
+		for (int i = 0; i < grid.getRows(); i++) {
+			for (int j = 0; j < grid.getCols(); j++) {
+				float cellX = grid.getX() + j*cellWidth;
+				float cellY = grid.getY() + i*cellWidth;
+				
+				boolean inX = cellX <= surface.mouseX && surface.mouseX < cellX + cellWidth;
+				boolean inY = cellY - cellWidth <= surface.mouseY && surface.mouseY < cellY;
+				
+				if (inX && inY) {
+					surface.fill(0, 255, 0);
+					surface.stroke(0, 255, 0);
+				}
+				
+				surface.rect(cellX, cellY, cellWidth, cellWidth);
+				surface.fill(255);
+				surface.stroke(255);
+			}
 		}
 	}
 }
