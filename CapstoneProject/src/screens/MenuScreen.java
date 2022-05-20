ed270f855cbd20be9491cf415dbad7428a243e0c
Antonio Cuan <antonio.j.cuan@gmail.com>
2022-05-20 10:21:50 -0700

    decorated menu

--- CapstoneProject/src/screens/MenuScreen.java
+++ CapstoneProject/src/screens/MenuScreen.java
@@ -24,12 +24,32 @@ public class MenuScreen extends Screen{
 	
 	public void draw() {
 		surface.background(255);
+		surface.rectMode(PConstants.CENTER);
+		surface.strokeWeight(2);
+		surface.fill(0, 200, 200);
+		surface.rect(WIDTH-200, 200, 100, 100);
+		surface.fill(200, 0, 255);
+		surface.rect(WIDTH-400, 600, 100, 100);
+		surface.fill(0, 200, 200);
+		surface.rect(WIDTH-100, 450, 100, 100);
+		surface.stroke(255,120,0);
+		surface.line(WIDTH-200, 200, 300, 300);
+		surface.line(WIDTH-400, 600, 200, 500);
+		surface.stroke(0);
+		surface.fill(200, 0, 0);
+		surface.circle(200, 500, 150);
+		surface.circle(100, 250, 150);
+		surface.fill(200, 200, 0);
+		surface.circle(300, 300, 75);
+		surface.circle(125, 100, 75);
+		surface.circle(150, 650, 75);
+		
 		surface.fill(0);
 		surface.textAlign(PConstants.CENTER);
 		surface.textSize(40);
 		surface.text("Polygon Protection", WIDTH/2, 100);
 		surface.fill(255, 150, 0);
-		surface.rectMode(PConstants.CENTER);
+		surface.stroke(0);
 		surface.rect(WIDTH/2, HEIGHT/2, 200, 50);
 		surface.fill(0);
 		surface.textSize(30);
